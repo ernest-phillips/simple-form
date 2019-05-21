620a96ca9bffbdbcbf8716430d684c30278a5e37
Ernest Phillips <ernestlphillips@gmail.com>
2019-05-21 14:52:58 -0700

    beginning of a beautiful form

---
+++ src/Reducers/FormReducer.js
@@ -0,0 +1,4 @@
+// import react, {Component} from 'react'
+import {createStore, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form'
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form'
+
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+    form: formReducer
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducer)
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById('root'));
+
